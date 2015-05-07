ae6d8c7de55cc57a42b45913e024f7bc239c938b
baldor_ <darkfrancky74@gmail.com>
2015-05-07 20:02:58 +0200

    added some comments

--- sphero-api/quickTest.sh
+++ sphero-api/quickTest.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+#Création de la lib
 make -B
+
+#Création de l'appli
 cd apps
 make -B
+
+#Lancement de l'appli
 cp Appli ../testAppLastBuild
 chmod 777 ../testAppLastBuild
 ../testAppLastBuild
