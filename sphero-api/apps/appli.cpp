b7fd9a577a92611b84d7ad98cd7f3f4c6704a783
baldor_ <darkfrancky74@gmail.com>
2015-05-08 17:49:49 +0200

    ergonomy reloaded

--- sphero-api/apps/appli.cpp
+++ sphero-api/apps/appli.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 int main(void)
 {
-	
-	cout << "\033]0;Sphero-cmd\007";
-	cout.flush();
-	
-	init();
 
-	string command("");
-	do
-	{
-        showHelp();
-		cout << "Sphero > ";
-		getline(cin, command);
-	}
-	while(handleCommand(command));
-	
-	
-	return 0;
+    cout << "\033]0;Sphero-cmd\007";
+    cout.flush();
+
+    init();
+
+    string command("");
+    showHelp();
+    do
+    {
+        cout << "Sphero > ";
+        getline(cin, command);
+    }
+    while(handleCommand(command));
+
+
+    return 0;
 }
